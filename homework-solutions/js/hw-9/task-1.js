9ed6c855d1c955e932a21fa1f5992b2c24fd34f7
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-01-20 00:39:53 +0200

    9-1 hw

---
+++ homework-solutions/js/hw-9/task-1.js
@@ -0,0 +1,31 @@
+/*
+ Имеется объект const character = { 'name': 'Barney', 'age': 36, 'gender': 'male', 'isQa': true }
+ 1. Создать массив из ключей объекта character и присвоить его в переменную "keyWithFourChars" т.е., где 4 буквы //name, isQa
+ 2. Создать массив из значений объекта character и присвоить его в переменную "stringValues" е, где тип данных строка //'Barney', 'male'
+ 3. Создать массив из ключей и значений объекта character и присвоить его в переменную "keyValuePairs", перебрать массив циклом for. 
+   На каждой итерации вывести пары ключ-значнение в виде `key = ${key}, value = ${value}`
+ 4. Проверить, есть ли в объекте ключ salary, результат присвоить в переменные "hasSalaryKey1stOption | hasSalaryKey2ndOption"
+   (Реализовать 2мя способами: через оператор "in" (1st) и "Object.hasOwn()" (2nd))
+*/
+
+const character = { 'name': 'Barney', 'age': 36, 'gender': 'male', 'isQa': true };
+
+// 1
+let keyWithFourChars;
+
+// 2
+let stringValues;
+
+// 3
+/* Do not touch this part */
+const logSpy = jest.spyOn(console, 'log');
+// ---
+
+// Ваш код
+let keyValuePairs;
+
+// 4
+let hasSalaryKey1stOption;
+let hasSalaryKey2ndOption;
+
+export { keyWithFourChars, stringValues, hasSalaryKey1stOption, hasSalaryKey2ndOption, keyValuePairs, logSpy };
