c0fcac7f33dd0bcebb68ffd4126d94c472ed0d7c
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-02-13 20:41:26 +0700

    Updated task-1

--- homework-solutions/js/hw-3/task-1.js
+++ homework-solutions/js/hw-3/task-1.js
@@ -80,7 +80,7 @@ item_6_type = typeof item_6;
 
 //21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
 
-console.log("item_6 == " + item_6 + ", " + "item_6_type == " + item_6_type);
+console.log(`item_6 == ${item_6}, item_6_type == ${item_6_type}`);
 
 //22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 
@@ -96,4 +96,4 @@ item_7_type = typeof item_7;
 
 //25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 
-console.log("item_7 == " + item_7 + ", " + "item_7_type == " + item_7_type);
+console.log(`item_7 == ${item_7}, item_7_type == ${item_7_type}`);
