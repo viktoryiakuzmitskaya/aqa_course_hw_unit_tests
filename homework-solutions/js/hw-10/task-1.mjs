960308bb48634a8273fccf91269bfb403c622440
Volodymyr Lazurenko <103571439+VladimirLQA@users.noreply.github.com>
2025-03-11 23:56:43 +0200

    Update task-1.mjs

--- homework-solutions/js/hw-10/task-1.mjs
+++ homework-solutions/js/hw-10/task-1.mjs
@@ -1,7 +1,7 @@
 /*
  1. Создайте объект qa с полями name, age, salary и методом getInfo(greetingsWord),
     который будет возвращать строку вида: 
-    ${greetingsWord}, my name is ${name}, i'm ${age} and my salary is ${salary}. 
+    ${greetingsWord}, my name is ${name}, I'm ${age} and my salary is ${salary}. 
     Значения в строке должны ссылаться на контекст ЭТОГО ОБЪЕКТА, без подмен.
 */
 
