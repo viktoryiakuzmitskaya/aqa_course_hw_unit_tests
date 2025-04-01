ab3212a81aac4458c4c390e53cd531d1fd406aca
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-04-01 18:57:35 +0700

    task-2 done

--- homework-solutions/ts/hw-14/task-2.ts
+++ homework-solutions/ts/hw-14/task-2.ts
@@ -4,3 +4,17 @@
 //    где каждый каждый элемент - это элемент исходного массива умноженный на его индекс
 //    Пример:
 //    map([1,2,3,4,5], callback) => [0,2,6,12,20]
+
+
+function customMap<T, U>(arr: T[], callback: (element: T, index: number, arr: T[]) => U): U[] {
+    const mappedArr: U[] = [];
+    for (let i = 0; i< arr.length; i++) {
+        let changedElement = callback(arr[i], i, arr);
+        mappedArr.push(changedElement);
+    }
+    return mappedArr;
+}
+
+console.log(customMap([1, 2, 3, 4, 5], (element, index) => element*index));
+
+
